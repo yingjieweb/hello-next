3e2167d75acca07cd8d04eb3cb29e93cd8c3bc72
yingjieweb <yingjieweb@qq.com>
2023-10-09 09:57:47 +0800

    feat: 🎸 Learn some new components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,33 @@
-import Link from 'next/link';
+import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+        {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload" // Load this particular script lazily during browser idle time.
+        onLoad={() =>
+          // run any JavaScript code immediately after the script has finished loading.
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   );
 }
