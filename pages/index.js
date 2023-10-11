19c94e6319a4ab0b13a179dfbcc84acff9880cc9
yingjieweb <yingjieweb@qq.com>
2023-10-11 10:31:02 +0800

    feat: 🎸 Learn pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+// components
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+// styles
 import utilStyles from "../styles/utils.module.css";
+// API
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +25,38 @@ export default function Home() {
           <a href="https://github.com/yingjieweb">Github</a>. 👀
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
